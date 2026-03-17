e51d0b89bf27e15de9d65c9d63b1ea74b82e7ff4
AbhishPulavarthi <pulavarthiabhish@gmail.com>
2026-03-17 15:33:44 +0530

    Application Entry & Welcome Message

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -5,7 +5,7 @@ public class BookMyStayApp {
         System.out.println("======================================");
         System.out.println("   Welcome to Book My Stay App");
         System.out.println("   Hotel Booking System v1.0");
-        System.out.println("======================================");
+        System.out.println("=====================================");
 
         System.out.println("Application started successfully.");
         System.out.println("Thank you for using Book My Stay!");
